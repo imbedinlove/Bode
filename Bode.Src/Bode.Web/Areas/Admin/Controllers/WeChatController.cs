b80077531451f8aed88ab3519269b805f4332cad
LcyEcho <imbedinlove@gmail.com>
2016-06-30 17:53:00 +0800

    添加微信回调方法

---
+++ Bode.Src/Bode.Web/Areas/Admin/Controllers/WeChatController.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+using System.Linq;
+using System.Web.Mvc;
+using Bode.Services.Core.Contracts;
+using Bode.Web.Areas.Admin.ViewModes;
+using OSharp.Core.Security;
+using OSharp.Web.Mvc.Security;
+using System.Threading.Tasks;
+using System.Web.Security;
+using OSharp.Utility.Data;
+using OSharp.Web.Mvc.UI;
+using System;
+using OSharp.Utility.Helper;
+using OSharp.Core.Data;
+using OSharp.Web.Mvc.Initialize;
+
+namespace Bode.Web.Areas.Admin.Controllers
+{
+    [Description("微信模块")]
+    //[BodeMenuGroupKey("Admin.Home")]
+    public class WeChatController : Controller
+    {
+
+        //[Authorize]
+        //[BodeMenuGroupKey("mk222")]
+        [Description("微信验证接口")]
+        public ActionResult NotiflyUrl()
+        {
+            string token = "imbedinlove";
+            if (string.IsNullOrEmpty(token))
+            {
+                return Content("");
+            }
+
+            string echoString = HttpContext.Request.QueryString["echoStr"];
+            string signature = HttpContext.Request.QueryString["signature"];
+            string timestamp = HttpContext.Request.QueryString["timestamp"];
+            string nonce = HttpContext.Request.QueryString["nonce"];
+
+            if (!string.IsNullOrEmpty(echoString))
+            {
+                HttpContext.Response.Write(echoString);
+                HttpContext.Response.End();
+            }
+            return Content(echoString);
+        }
+    }
+}
